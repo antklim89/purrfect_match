8cdaf23298de2421054d95d6c604ed7b55eac546
Anton <antklim89@mail.ru>
2026-08-12 09:04:46 +0000

    feat: update error component
    add actions
    update styles

--- apps/web/src/shared/ui/error-component.tsx
+++ apps/web/src/shared/ui/error-component.tsx
@@ -1,15 +1,18 @@
+import type { ReactNode } from 'react';
 import { AlertTriangleIcon } from 'lucide-react';
 
-import { Alert, AlertDescription, AlertTitle } from './alert';
+import { Alert, AlertAction, AlertDescription, AlertTitle } from './alert';
 
 export function ErrorComponent({
   title,
   message,
   status = 500,
+  action,
 }: {
   title?: string;
   message?: string;
   status?: number;
+  action?: ReactNode;
 }) {
   const titleMap = {
     500: 'Unexpected Error',
@@ -21,9 +24,10 @@ export function ErrorComponent({
 
   return (
     <Alert className="border-red-950 dark:border-red-400 bg-destructive text-destructive-foreground">
-      <AlertTriangleIcon />
-      <AlertTitle className="text-white">{title ?? titleMap[status] ?? titleMap[500]}</AlertTitle>
+      <AlertTriangleIcon className="size-8" />
+      <AlertTitle className="text-white text-2xl">{title ?? titleMap[status] ?? titleMap[500]}</AlertTitle>
       <AlertDescription className="text-white">{message ?? 'Unexpected error. Try again later.'}</AlertDescription>
+      <AlertAction className="p-2">{action}</AlertAction>
     </Alert>
   );
 }
