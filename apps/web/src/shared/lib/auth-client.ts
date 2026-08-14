de2af561e70e2ea64b42c61c80652c87eee1ef1c
Anton <antklim89@mail.ru>
2026-08-14 10:01:05 +0000

    add automatic header import

--- apps/web/src/shared/lib/auth-client.ts
+++ apps/web/src/shared/lib/auth-client.ts
@@ -2,9 +2,18 @@ import { createAuthClient } from 'better-auth/react';
 
 import { env } from './env';
 
+const baseURL = typeof window === 'undefined' ? env.API_URL : env.WEB_URL;
+
 export const authClient = createAuthClient({
-  baseURL: typeof window === 'undefined' ? env.API_URL : env.WEB_URL,
+  baseURL,
   sessionOptions: {
     refetchOnWindowFocus: false,
+  },
+  fetchOptions: {
+    async onRequest(context) {
+      if (typeof window === 'undefined') {
+        context.headers = await import('next/headers').then(m => m.headers());
+      }
+    },
   },
 });
