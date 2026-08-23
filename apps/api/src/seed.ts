1a61e6fdf2f0f81d74de58aa3c518f4b6f1bb060
Anton <antklim89@mail.ru>
2026-08-23 20:43:40 +0300

    update

--- apps/api/src/seed.ts
+++ apps/api/src/seed.ts
@@ -8,8 +8,33 @@ import { auth } from '@/lib/auth';
 import { db } from '@/lib/db';
 import * as schema from '@/schema';
 
-const USERS_NUMBER = 2;
-const ADS_NUMBER = 5;
+const USERS_NUMBER = 20;
+const ADS_NUMBER = 50;
+const contacts = [
+  { number: '1 (555) 555 44 55', type: 'phone' },
+  { number: '1 (555) 555 33 55', type: 'whatsapp' },
+  { number: '4 (999) 555 22 55', type: 'telegram' },
+  { number: '71 (888) 555 11 55', type: 'viber' },
+];
+
+const types = ['dog', 'cat', 'bird', 'hamster', 'horse', 'rabbit', 'fish', 'turtle', 'duck', 'snake', 'lizard'];
+
+const breeds = [
+  'Bulldog',
+  'Poodle',
+  'Beagle',
+  'Dachshund',
+  'German Shepherd Dog',
+  'Golden Retriever',
+  'Bichon Frise',
+  'Labrador',
+  'Pug',
+  'Boxer',
+  'Rottweiler',
+  'Goldendoodle',
+  'Border Collie',
+  'Great Dane',
+];
 
 export const PLACEHOLDER_BLUR_DATA =
   'data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPk4vpvDAACgQFIuAF96wAAAABJRU5ErkJggg==';
@@ -25,28 +50,29 @@ async function resetDb() {
 }
 
 async function createUsers() {
+  await auth.api.signUpEmail({ body: { email: 'admin@mail.com', name: 'Admin', password: 'qwer1234' } });
+
   for (let index = 0; index < USERS_NUMBER; index++) {
-    const ctx = await auth.$context;
-    const user = await ctx.test.saveUser(ctx.test.createUser());
-    await ctx.test.login({ userId: user.id });
+    await auth.api.signUpEmail({
+      body: { email: faker.internet.email(), name: faker.person.firstName(), password: 'qwer1234' },
+    });
   }
 }
 
 async function createAd() {
   const users = await db.query.userTable.findMany({ columns: { id: true } });
-  const breeds = faker.helpers.multiple(() => faker.lorem.word(), { count: 9 });
-  const type = faker.helpers.multiple(() => faker.animal.type(), { count: 9 });
 
   const ads: AdInsertType[] = Array.from({ length: ADS_NUMBER }, () => {
     return {
       id: faker.string.uuid({ version: 7 }),
       name: faker.animal.petName(),
       breed: faker.helpers.arrayElement(breeds),
-      type: faker.helpers.arrayElement(type),
-      description: faker.lorem.sentence({ min: 20, max: 2000 }).slice(0, 38000),
+      type: faker.helpers.arrayElement(types),
+      description: faker.lorem.sentence({ min: 20, max: 1000 }).slice(0, 38000),
       price: faker.number.float({ min: 0, max: 1000000, multipleOf: 0.02 }),
       userId: faker.helpers.arrayElement(users).id,
       createdAt: faker.date.past({ years: 7 }).toISOString(),
+      contacts: faker.helpers.arrayElements(contacts),
       isPublished: true,
     };
   });
@@ -59,7 +85,7 @@ async function createAdImages() {
   const adImages = await Array.fromAsync(new Bun.Glob('*').scan({ onlyFiles: true, cwd: 'media/development/ads' }));
 
   const adsImages: AdImageInsertType[] = ads.flatMap(ad =>
-    faker.helpers.arrayElements(adImages).flatMap(adImageSrc => {
+    faker.helpers.arrayElements(adImages, { min: 2, max: 6 }).flatMap(adImageSrc => {
       return {
         url: `/media/development/ads/${adImageSrc}`,
         adId: ad.id,
