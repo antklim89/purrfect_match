52a68ce8a2cb81dd61ae8ca4e3a0a4ff52b104b6
Anton <antklim89@mail.ru>
2026-08-23 16:30:06 +0300

    fix: add ComponentProps

--- apps/web/src/shared/ui/nav-link.tsx
+++ apps/web/src/shared/ui/nav-link.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import type { ComponentProps } from 'react';
 import type { Route } from 'next';
 import Link, { type LinkProps } from 'next/link';
 import { usePathname } from 'next/navigation';
 
-export function NavLink({ className, activeClassName, ...props }: LinkProps<Route> & { activeClassName?: string }) {
+export function NavLink({
+  className,
+  activeClassName,
+  ...props
+}: LinkProps<Route> & ComponentProps<typeof Link> & { activeClassName?: string }) {
   const pathname = usePathname();
   const isActive = pathname === props.href;
 
