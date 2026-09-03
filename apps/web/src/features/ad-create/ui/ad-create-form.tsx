814de455a00073db897f324b75cc6add6314ca9a
Anton <antklim89@mail.ru>
2026-09-03 10:18:11 +0000

    feat: checking animal type and breed

--- apps/web/src/features/ad-create/ui/ad-create-form.tsx
+++ apps/web/src/features/ad-create/ui/ad-create-form.tsx
@@ -1,3 +1,5 @@
+import { animalBreeds, animalTypes } from '@purrfect_match/shared/entities/animal/constants';
+import type { AnimalTypes } from '@purrfect_match/shared/entities/animal/types';
 import { toast } from 'sonner';
 
 import { deleteImageDraftAd, uploadImageDraftAd } from '@/shared/api/ads';
@@ -10,15 +12,15 @@ export function AdCreateForm() {
   const form = useTypedAppFormContext(adCreateFormOptions);
 
   async function handleImageUpload(image: File) {
-    const { data: uploadedImage, error } = await uploadImageDraftAd({ image });
-    if (!error) return uploadedImage;
-    toast.error(error.message);
+    const result = await uploadImageDraftAd({ image });
+    if (result.error) toast.error(result.error.message);
+    return result;
   }
 
   async function handleImageRemove(image: FileUploadFileType) {
-    const { error } = await deleteImageDraftAd({ adId: image.id });
-    if (!error) return;
-    toast.error(error.message);
+    const result = await deleteImageDraftAd({ adId: image.id });
+    if (result.error) toast.error(result.error.message);
+    return result;
   }
 
   return (
@@ -26,11 +28,47 @@ export function AdCreateForm() {
       <form.AppField name="name">
         {field => <field.FormInput placeholder="Enter animal name" label="Name" errors={field.state.meta.errors} />}
       </form.AppField>
-      <form.AppField name="type">
-        {field => <field.FormInput placeholder="Enter animal type" label="Type" errors={field.state.meta.errors} />}
+      <form.AppField name="type" listeners={{ onChange: ({ fieldApi }) => fieldApi.form.setFieldValue('breed', '') }}>
+        {field => (
+          <field.FormSelect
+            className="capitalize"
+            placeholder="Select animal type"
+            label="Type"
+            errors={field.state.meta.errors}
+          >
+            {animalTypes.map(animalType => (
+              <field.FormSelectItem className="capitalize" key={animalType} value={animalType}>
+                {animalType}
+              </field.FormSelectItem>
+            ))}
+          </field.FormSelect>
+        )}
       </form.AppField>
       <form.AppField name="breed">
-        {field => <field.FormInput placeholder="Enter animal breed" label="Breed" errors={field.state.meta.errors} />}
+        {field => (
+          <form.Subscribe
+            selector={state =>
+              typeof state.values.type === 'string' ? animalBreeds[state.values.type as AnimalTypes] : null
+            }
+          >
+            {(selectedAnimalBreeds) =>
+              selectedAnimalBreeds && (
+                <field.FormSelect
+                  className="capitalize"
+                  placeholder="Select animal breed"
+                  label="Breed"
+                  errors={field.state.meta.errors}
+                >
+                  {selectedAnimalBreeds.map(animalType => (
+                    <field.FormSelectItem className="capitalize" key={animalType} value={animalType}>
+                      {animalType}
+                    </field.FormSelectItem>
+                  ))}
+                </field.FormSelect>
+              )
+            }
+          </form.Subscribe>
+        )}
       </form.AppField>
       <form.AppField name="description">
         {field => (
@@ -44,16 +82,18 @@ export function AdCreateForm() {
       <form.AppField name="images">
         {field => (
           <Field>
-            <FieldLabel htmlFor={field.name + field.form.formId}>Upload Images</FieldLabel>
+            <FieldLabel htmlFor={field.name + field.form.formId}>
+              Upload Images ({field.state.value.length} / {20})
+            </FieldLabel>
             <FileUpload
               id={field.name + field.form.formId}
               onFileRemove={async (image, index) => {
-                await handleImageRemove(image);
-                field.removeValue(index);
+                const { error } = await handleImageRemove(image);
+                if (!error) field.removeValue(index);
               }}
               images={field.state.value}
               onFileChange={async image => {
-                const uploadedImage = await handleImageUpload(image);
+                const { data: uploadedImage } = await handleImageUpload(image);
                 if (uploadedImage) field.handleChange(prev => [...prev, uploadedImage]);
               }}
               placeholder="Upload images"
--- packages/shared/entities/ad/schemas.ts
+++ packages/shared/entities/ad/schemas.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod/v4-mini';
 
-import { ADS_SORT_BY, MAX_ADS_LIMIT } from './constants';
+import { ADS_SORT_BY, MAX_ADS_LIMIT, MAX_IMAGES_PER_AD } from './constants';
+import { animalBreeds, animalTypes } from '../animal/constants';
 
 export const AdImageSchema = z.object({
   id: z.uuid(),
@@ -8,14 +9,21 @@ export const AdImageSchema = z.object({
   blurDataUrl: z.string(),
 });
 
-export const AdPublishSchema = z.object({
-  name: z.string().check(z.minLength(2), z.maxLength(500)),
-  type: z.string().check(z.minLength(2), z.maxLength(500)),
-  price: z.number().check(z.minimum(0), z.maximum(9000000)),
-  description: z.string().check(z.minLength(10), z.maxLength(40000)),
-  breed: z.string().check(z.minLength(2), z.maxLength(500)),
-  images: z.array(AdImageSchema).check(z.minLength(1), z.maxLength(20)),
-});
+export const AdPublishSchema = z
+  .object({
+    name: z.string().check(z.minLength(2), z.maxLength(500)),
+    type: z.enum(animalTypes, 'Unknown animal type.'),
+    price: z.number().check(z.minimum(0), z.maximum(9000000)),
+    description: z.string().check(z.minLength(10), z.maxLength(40000)),
+    breed: z.string(),
+    images: z
+      .array(AdImageSchema)
+      .check(
+        z.minLength(1, 'Add at least one image.'),
+        z.maxLength(MAX_IMAGES_PER_AD, `Max allowed images is ${MAX_IMAGES_PER_AD}`),
+      ),
+  })
+  .check(z.refine(v => animalBreeds[v.type].includes(v.breed), { path: ['breed'], error: 'Unknown animal breed.' }));
 
 export const AdDraftSchema = z.partial(
   z.object({
